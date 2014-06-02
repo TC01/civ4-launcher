7badf8d087c5b1892905175e5da6d83dd74989d5
Ben Rosser <rosser.bjr@gmail.com>
2014-06-02 12:23:15 -0400

    Lots of cleanup needed, but here's the current state of the project.

---
+++ Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Civilization 4 Vanilla", "Warlords", "Beyond the Sword", "Civilization IV Colonization"})
+        Me.ComboBox1.Location = New System.Drawing.Point(75, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(229, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(75, 52)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(229, 21)
+        Me.ComboBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Civ Version:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Mod Name:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 112)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(97, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Launch Selected"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(219, 112)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(85, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Reset Settings"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 84)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(111, 17)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Close after launch"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(129, 84)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(166, 17)
+        Me.CheckBox2.TabIndex = 7
+        Me.CheckBox2.Text = "Remember last mod launched"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(115, 112)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(98, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Launch Random" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(313, 151)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(329, 189)
+        Me.MinimumSize = New System.Drawing.Size(329, 189)
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Civ IV Mod Launcher"
+        Me.TransparencyKey = System.Drawing.Color.Blue
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,343 @@
+﻿'Civilization IV Mod Launcher by TC01
+'Launches mods for Civ 4, its expansions, and Colonization
+'Tries to get the install dir from the registry, if this fails, prompts the user
+'Writes the install dir to My.Settings, along with whether or not all versions of Civ are installed
+'Does NOT support My Documents\My Games mods.
+
+'As of Version 2, supports Gold and Complete installation paths
+
+Public Class Form1
+
+    Private CivilizationPath, ColonizationPath, WarlordsPath, BTSPath As String
+    Private RegRoot As String = "Firaxis Games\"
+    Private LocalMachine As String = "HKEY_LOCAL_MACHINE"
+    Private CurrentUser As String = "HKEY_CURRENT_USER"
+    Private GoldKey As String = "Sid Meier's Civilization 4 Gold"
+    Private CompleteKey As String = "Sid Meier's Civilization 4 Complete"
+
+    'Form load function. Used to get registry paths, if registry paths fail, provide prompt for manual install. Also exclude Civ versions not present on local machine.
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim NoCiv As Boolean = My.Settings.NoCiv
+        Dim NoWarlords As Boolean = My.Settings.NoWarlords
+        Dim NoBTS As Boolean = My.Settings.NoBTS
+        Dim NoCol As Boolean = My.Settings.NoCol
+        Dim CivInputPath As String = My.Settings.OtherCivPath
+        Dim WarlordsInputPath As String = My.Settings.OtherWarlordsPath
+        Dim BTSInputPath As String = My.Settings.OtherBTSPath
+        Dim ColInputPath As String = My.Settings.OtherColPath
+
+        'Check for Civ 4 first
+        If NoCiv = False Then
+            If CivInputPath = "" Then
+                CivilizationPath = FindInstallPath("", "Sid Meier's Civilization 4", "Mod Launcher has failed to detect your Civ install path. If Civ 4 is installed, find your install path manually and place it in the prompt below. If Civ 4 is not installed, press OK and continue.", "Civ 4 is not installed.")
+                If CivilizationPath = "" Then
+                    My.Settings.NoCiv = True
+                    My.Settings.NoWarlords = True
+                    My.Settings.NoBTS = True
+                    NoCiv = True
+                    NoWarlords = True
+                    NoBTS = True
+                Else
+                    My.Settings.OtherCivPath = CivilizationPath
+                End If
+            Else
+                CivilizationPath = CivInputPath & "\"
+            End If
+
+            'If we found Civ 4, keep checking for the expansions (warlords and BTS)
+            If NoWarlords = False Then
+                If WarlordsInputPath = "" Then
+                    WarlordsPath = FindInstallPath("\Warlords", "Sid Meier's Civilization 4 - Warlords", "Mod Launcher has failed to detect your Warlords install path. If Warlords is installed, find your install path manually and place it in the prompt below. If Warlords is not installed, press OK and continue.", "Warlords is not installed.")
+                    If WarlordsPath = "" Then
+                        My.Settings.NoWarlords = True
+                        NoWarlords = True
+                    Else
+                        My.Settings.OtherWarlordsPath = WarlordsPath
+                    End If
+                Else
+                    WarlordsPath = WarlordsInputPath & "\"
+                End If
+            End If
+            If NoBTS = False Then
+                If BTSInputPath = "" Then
+                    BTSPath = FindInstallPath("\Beyond the Sword", "Sid Meier's Civilization 4 - Beyond the Sword", "Mod Launcher has failed to detect your Beyond the Sword install path. If Beyond the Sword is installed, find your install path manually and place it in the prompt below. If Beyond the Sword is not installed, press OK and continue.", "Beyond the Sword is not installed.")
+                    If BTSPath = "" Then
+                        My.Settings.NoBTS = True
+                        NoBTS = True
+                    Else
+                        My.Settings.OtherBTSPath = BTSPath
+                    End If
+                Else
+                    BTSPath = BTSInputPath & "\"
+                End If
+            End If
+        End If
+
+        'Now, check for Colonization (no dependencies)
+        If NoCol = False Then
+            If ColInputPath = "" Then
+                ColonizationPath = FindInstallPath("\Colonization", "Sid Meier's Civilization IV Colonization", "Mod Launcher has failed to detect your Colonization install path. If Colonization is installed, find your install path manually and place it in the prompt below. If Colonization is not installed, press OK and continue.", "Colonization is not installed.")
+                If ColonizationPath = "" Then
+                    My.Settings.NoCol = True
+                    NoCol = True
+                Else
+                    My.Settings.OtherColPath = ColonizationPath
+                End If
+            Else
+                ColonizationPath = CivInputPath & "\"
+            End If
+        End If
+
+        'Remove the absent versions from the Combo Box
+        If NoCiv = True Then
+            ComboBox1.Items.Remove("Civilization 4 Vanilla")
+        End If
+        If NoWarlords = True Then
+            ComboBox1.Items.Remove("Warlords")
+        End If
+        If NoBTS = True Then
+            ComboBox1.Items.Remove("Beyond the Sword")
+        End If
+        If NoCol = True Then
+            ComboBox1.Items.Remove("Civilization IV Colonization")
+        End If
+
+        'Set checkbox settings
+        CheckBox1.Checked = My.Settings.CloseOnLaunch
+        CheckBox2.Checked = My.Settings.Remember
+        If My.Settings.Remember = True Then
+            ComboBox1.Text = My.Settings.LastGameLaunched
+            ComboBox2.Text = My.Settings.LastModLaunched
+        End If
+
+        Randomize()
+
+    End Sub
+
+    'Called on combo box selection change. Updates list of available mods per different Civ version.
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim CivVersion As String = ComboBox1.GetItemText(ComboBox1.SelectedItem())
+        Dim ModsList As System.Collections.ObjectModel.ReadOnlyCollection(Of String)
+        Dim CivModString As String
+        Dim CivModStart, OldModsList As Integer
+        OldModsList = ComboBox2.Items.Count()
+        ComboBox2.Items.Clear()
+
+        'Get the list of mods (default vanilla)
+        ModsList = My.Computer.FileSystem.GetDirectories(CivilizationPath & "Mods\")
+        If CivVersion = "Warlords" Then
+            ModsList = My.Computer.FileSystem.GetDirectories(WarlordsPath & "Mods\")
+        ElseIf CivVersion = "Beyond the Sword" Then
+            ModsList = My.Computer.FileSystem.GetDirectories(BTSPath & "Mods\")
+        ElseIf CivVersion = "Civilization IV Colonization" Then
+            ModsList = My.Computer.FileSystem.GetDirectories(ColonizationPath & "Mods\")
+        End If
+
+        ComboBox2.Items.Add("Regular " & CivVersion)        'So we can start up unmodded Civ versions too!
+        For CivMod As Integer = 0 To ModsList.Count() - 1 Step 1
+            CivModString = ModsList.Item(CivMod)
+            CivModStart = CivModString.LastIndexOf("\")
+            CivModString = CivModString.Substring(CivModStart + 1)
+            ComboBox2.Items.Add(CivModString)
+        Next
+
+    End Sub
+
+    'Called when the mod launch button is clicked. Uses Shell() to launch the given Civ version and mod version.
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim CivVersion As String = ComboBox1.GetItemText(ComboBox1.SelectedItem())
+        Dim ModName As String = ComboBox2.GetItemText(ComboBox2.SelectedItem())
+        'Do the actual running of the program: through Shell()
+        If CivVersion = "Civilization 4 Vanilla" Then
+            If ModName = "Regular Civilization 4 Vanilla" Then
+                LaunchMod("""" & CivilizationPath & "Civilization4.exe""", ModName)
+            Else
+                LaunchMod("""" & CivilizationPath & "Civilization4.exe"" mod=\" & ModName, ModName)
+            End If
+        ElseIf CivVersion = "Warlords" Then
+            If ModName = "Regular Warlords" Then
+                LaunchMod("""" & WarlordsPath & "Civ4Warlords.exe""", ModName)
+            Else
+                LaunchMod("""" & WarlordsPath & "Civ4Warlords.exe"" mod=\" & ModName, ModName)
+            End If
+        ElseIf CivVersion = "Beyond the Sword" Then
+            If ModName = "Regular Beyond the Sword" Then
+                LaunchMod("""" & BTSPath & "\Civ4BeyondSword.exe""", ModName)
+            Else
+                LaunchMod("""" & BTSPath & "\Civ4BeyondSword.exe"" mod=\" & ModName, ModName)
+            End If
+        ElseIf CivVersion = "Civilization IV Colonization" Then
+            If ModName = "Regular Civilization IV Colonization" Then
+                LaunchMod("""" & ColonizationPath & "\Colonization.exe""", ModName)
+            Else
+                LaunchMod("""" & ColonizationPath & "\Colonization.exe"" mod=\" & ModName, ModName)
+            End If
+        End If
+    End Sub
+
+    'Called when the reset all button is clicked, resets all settings variables to False (for bools) and "" (for strings)
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        My.Settings.NoCiv = False
+        My.Settings.NoWarlords = False
+        My.Settings.NoBTS = False
+        My.Settings.NoCol = False
+        My.Settings.OtherCivPath = ""
+        My.Settings.OtherWarlordsPath = ""
+        My.Settings.OtherBTSPath = ""
+        My.Settings.OtherColPath = ""
+        My.Settings.CloseOnLaunch = False
+        My.Settings.Remember = False
+        My.Settings.LastModLaunched = ""
+        My.Settings.LastGameLaunched = ""
+        MsgBox("All settings have been cleared!", MsgBoxStyle.OkOnly, "Mod Launcher")
+    End Sub
+
+    'Launch when the random mod button is clicked
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim CivVersion As String = ComboBox1.GetItemText(ComboBox1.SelectedItem())
+        Dim ModName As String
+        Dim ModNum As Integer
+        Dim Result As MsgBoxResult
+
+        'Fix a possible unhandled exception
+        If CivVersion = Nothing Then
+            Return
+        End If
+
+        'Get a random modm set a combo box, prompt the user
+        ModNum = CInt(Int(((ComboBox2.Items.Count() - 1) - 0 + 1) * Rnd() + 0))
+        ModName = ComboBox2.Items.Item(ModNum)
+        ComboBox2.Text = ModName
+        Result = MsgBox("Launching " & ModName & " as random mod.", vbOKCancel, "Mod Launcher")
+        If Result = vbCancel Then
+            Return
+        End If
+
+        'Do the actual running of the program: through Shell()
+        If CivVersion = "Civilization 4 Vanilla" Then
+            If ModName = "Regular Civilization 4 Vanilla" Then
+                LaunchMod("""" & CivilizationPath & "Civilization4.exe""", ModName)
+            Else
+                LaunchMod("""" & CivilizationPath & "Civilization4.exe"" mod=\" & ModName, ModName)
+            End If
+        ElseIf CivVersion = "Warlords" Then
+            If ModName = "Regular Warlords" Then
+                LaunchMod("""" & WarlordsPath & "Civ4Warlords.exe""", ModName)
+            Else
+                LaunchMod("""" & WarlordsPath & "Civ4Warlords.exe"" mod=\" & ModName, ModName)
+            End If
+        ElseIf CivVersion = "Beyond the Sword" Then
+            If ModName = "Regular Beyond the Sword" Then
+                LaunchMod("""" & BTSPath & "\Civ4BeyondSword.exe""", ModName)
+            Else
+                LaunchMod("""" & BTSPath & "\Civ4BeyondSword.exe"" mod=\" & ModName, ModName)
+            End If
+        ElseIf CivVersion = "Civilization IV Colonization" Then
+            If ModName = "Regular Civilization IV Colonization" Then
+                LaunchMod("""" & ColonizationPath & "\Colonization.exe""", ModName)
+            Else
+                LaunchMod("""" & ColonizationPath & "\Colonization.exe"" mod=\" & ModName, ModName)
+            End If
+        End If
+    End Sub
+
+    'Change settings for last mod launched
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        My.Settings.CloseOnLaunch = CheckBox1.Checked
+    End Sub
+
+    'Change settings for whether or not to save launch data
+    Private Sub CheckBox2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox2.CheckedChanged
+        My.Settings.Remember = CheckBox2.Checked
+    End Sub
+
+    'Might be necessary, might not be... but just to be sure, saving settings
+    Private Sub Form1_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        My.Settings.Save()
+    End Sub
+
+    'End of Form1 events, beginning of internal functions and subroutines
+
+    'Finds the path of the given civ version (reg key) passed to it
+    Private Function FindInstallPath(ByVal CivType As String, ByVal RegPath As String, ByVal ErrorMsg As String, ByVal DefaultResponse As String)
+        Dim Valid As Boolean = True
+        Dim Path As String = ""
+        Dim DialogPath As String = ""
+        Dim RegKey As Object
+        Dim RCurrentUser, RLocalMachine As String
+        Dim OSType As String
+
+        'Bugfix to check for x64 systems
+        If IntPtr.Size = 8 Then
+            OSType = "\SOFTWARE\Wow6432Node\"
+        Else
+            OSType = "\Software\"
+        End If
+
+        'Construct the root of both registry strings
+        RLocalMachine = LocalMachine & OSType & RegRoot
+        RCurrentUser = CurrentUser & OSType & RegRoot
+
+        'Old buggy code to do this
+        'RLocalMachine = LocalMachine & RegRoot
+        'RCurrentUser = CurrentUser & RegRoot
+
+        'First standard, gold, and complete registries on local machine
+        RegKey = My.Computer.Registry.GetValue(RLocalMachine & RegPath, "INSTALLDIR", Nothing)
+        If CStr(RegKey) = "" Then
+            Valid = False
+            RegKey = My.Computer.Registry.GetValue(RLocalMachine & GoldKey, "INSTALLDIR" & CivType, Nothing)
+            If CStr(RegKey) = "" Then
+                RegKey = My.Computer.Registry.GetValue(RLocalMachine & CompleteKey, "INSTALLDIR" & CivType, Nothing)
+                If CStr(RegKey) <> "" Then
+                    Valid = True
+                End If
+            Else
+                Valid = True
+            End If
+        End If
+
+        'Then try registries on current user if the others failed
+        If Valid = False Then
+            RegKey = My.Computer.Registry.GetValue(RCurrentUser & RegPath, "INSTALLDIR", Nothing)
+            If CStr(RegKey) = "" Then
+                RegKey = My.Computer.Registry.GetValue(RCurrentUser & GoldKey, "INSTALLDIR" & CivType, Nothing)
+                If CStr(RegKey) = "" Then
+                    RegKey = My.Computer.Registry.GetValue(RCurrentUser & CompleteKey, "INSTALLDIR" & CivType, Nothing)
+                    If CStr(RegKey) <> "" Then
+                        Valid = True
+                    End If
+                Else
+                    Valid = True
+                End If
+            Else
+                Valid = True
+            End If
+        End If
+
+        'If nothing worked, make an input box, and save either the registry result or the input result to path
+        If Valid = False Then
+            DialogPath = InputBox(ErrorMsg, "Mod Launcher", DefaultResponse)
+            If DialogPath <> DefaultResponse Then
+                Path = DialogPath & "\"
+            End If
+        Else
+            Path = CStr(RegKey) & "\"
+        End If
+
+        Return Path
+    End Function
+
+    'Launches a mod and perform various special tasks
+    Private Sub LaunchMod(ByVal Command As String, ByVal ModName As String)
+        Shell(Command)
+        If My.Settings.Remember = True Then
+            My.Settings.LastGameLaunched = ComboBox1.GetItemText(ComboBox1.SelectedItem)
+            My.Settings.LastModLaunched = ModName
+        End If
+        If My.Settings.CloseOnLaunch = True Then
+            Me.Close()
+        End If
+    End Sub
+
+End Class
